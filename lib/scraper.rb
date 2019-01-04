6b54177cd235a29342cf4d563926aed9ed818c70
Brian Rosengarten <barosengarten@gmail.com>
2019-01-04 00:02:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,29 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
+  attr_accessor :students
+
   def self.scrape_index_page(index_url)
-    
+    page = Nokogiri::HTML(open(index_url))
+
+    students = []
+
+    page.css("div.student-card").each do |student|
+      name = student.css("h4.student-name").text
+      location = student.css("p.student-location").text
+      profile_url = student.css("a").attribute("href").value
+    students << {name: name, location: location, profile_url: profile_url}
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
 
+ Scraper.scrape_index_page("./fixtures/student-site/index.html")
